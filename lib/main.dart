3480f824cca6b2b4e8c8187ced8e481cff17264a
Abhi Bhullar <76940733+userabhibhullar@users.noreply.github.com>
2023-10-28 13:44:35 +0530

    update ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:formulistic/screens/home.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const Home(),
+        theme: ThemeData(
+          fontFamily: 'InriaSans',
+          colorScheme: const ColorScheme.dark(
+              primary: Colors.white,
+              background: Colors.black,
+              secondary: Color.fromRGBO(254, 114, 16, 1.0),
+              secondaryContainer: Color.fromRGBO(245, 214, 214, 1.0),
+              onSecondaryContainer: Color.fromRGBO(30, 22, 52, 1.0)),
+        ));
   }
 }
---
+++ lib/screens/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color primary = Theme.of(context).colorScheme.primary;
+    Color background = Theme.of(context).colorScheme.background;
+    Color secondary = Theme.of(context).colorScheme.secondary;
+    Color secondaryContainer = Theme.of(context).colorScheme.secondaryContainer;
+    Color secondaryContainerText =
+        Theme.of(context).colorScheme.onSecondaryContainer;
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: background,
+          foregroundColor: primary,
+          title: const Text(
+            "FORMULISTIC",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+          ),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Column(
+          children: [
+            Container(height: 2, color: primary),
+            Padding(
+                padding: const EdgeInsets.only(top: 12, left: 12, right: 12),
+                child: Directionality(
+                  textDirection: TextDirection.ltr,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 3, color: primary),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: primary,
+                      ),
+                      hintText: "type to search...",
+                    ),
+                  ),
+                ))
+          ],
+        ));
+  }
+}
